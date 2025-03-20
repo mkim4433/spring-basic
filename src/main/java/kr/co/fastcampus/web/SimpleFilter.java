d14ac896c7467bfda94afb4e1d65cc8c3a9b1a1f
mkim4433 <mkim4433@gmail.com>
2025-03-20 16:01:53 +0900

    어노테이션 WebServlet, WebFilter 로 web.xml 설정 변경

--- src/main/java/kr/co/fastcampus/web/SimpleFilter.java
+++ src/main/java/kr/co/fastcampus/web/SimpleFilter.java
@@ -3,10 +3,15 @@ package kr.co.fastcampus.web;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebFilter(
+        filterName = "simpleFilter",
+        urlPatterns = "simple"
+)
 @Slf4j
 public class SimpleFilter implements Filter {
     @Override
