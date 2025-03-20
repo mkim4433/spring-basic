f987c1ad10cda7c84ebe5b0dcecaf3dcd96138e0
mkim4433 <mkim4433@gmail.com>
2025-03-20 15:39:03 +0900

    filter, session 코드 추가

---
+++ src/main/java/kr/co/fastcampus/web/SimpleFilter.java
@@ -0,0 +1,32 @@
+package kr.co.fastcampus.web;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class SimpleFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain chain) throws IOException, ServletException {
+        log.info("Filter -- Hello World ++");
+        var request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        Object userNameObj = session.getAttribute("userName");
+        var userName = (String) userNameObj;
+        if (userName == null) {
+            log.info("NEW user");
+            session.setAttribute("userName", "mkim");
+        } else {
+            log.info("user : " + userName);
+        }
+
+        chain.doFilter(servletRequest, servletResponse);
+        var writer = servletResponse.getWriter();
+        writer.println("filter - Hello World!!");
+    }
+}
