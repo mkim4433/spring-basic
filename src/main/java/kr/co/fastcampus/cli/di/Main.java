51300b533ad3eecf100a81596835c192d43212b2
mkim4433 <mkim4433@gmail.com>
2025-03-22 19:52:08 +0900

    의존성 주입 코드를 IoC컨테이너로 이전해서 의존성 역전

--- src/main/java/kr/co/fastcampus/cli/di/Main.java
+++ src/main/java/kr/co/fastcampus/cli/di/Main.java
@@ -1,10 +1,13 @@
 package kr.co.fastcampus.cli.di;
 
+import kr.co.fastcampus.cli.Dao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        boolean condition = true;
-        B b = new B(condition);
-        A a = new A(b);
+        ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+        A a = context.getBean("a", A.class);
         a.print();
     }
 }
